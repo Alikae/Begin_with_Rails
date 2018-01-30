0afec39f5bd904c7b2979f08d839d3d1500f57aa
Alikae <alikae@laposte.net>
2018-01-30 13:54:13 +0100

    Amelioration1

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def about_you
+  end
+
+  def about_group
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/about'
+
+  root to: 'static_pages#home'
+
+  get '/contact', to: 'static_pages#contact', :as => 'contact'
+
+  get '/about', to: 'static_pages#about', :as => 'about'
+
+  get '/about/Alikae', to: 'static_pages#about_you', :as => 'about_alikae'
+
+  get '/about/niPutsNiSoumises', to: 'static_pages#about_group', :as => 'about_group'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
